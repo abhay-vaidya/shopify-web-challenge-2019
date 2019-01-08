b5ce1e418dbc30b5193dad093c8efe1191c03e1b
abhay-vaidya <abhay6547@gmail.com>
2019-01-08 13:13:14 -0500

    Clean up item actions

--- src/redux/actions/item.actions.ts
+++ src/redux/actions/item.actions.ts
@@ -1,7 +1,28 @@
 import { itemConstants } from '../constants/index'
 import { itemService } from '../../services'
-import store from '../store'
 import he from 'he'
+
+function transformRawItem(item, index) {
+  const { body, category, title, keywords } = item
+
+  // Body comes in as encoded HTML, we must decode it to properly render
+  const decodedBody = he.decode(body)
+
+  // Split keywords by comma and remove any leading or trailing whitespace
+  const keywordsArray = keywords.split(',').map((keyword) => {
+    return keyword.trim()
+  })
+  const keywordsSet = new Set(keywordsArray)
+
+  return {
+    id: index,
+    title,
+    category,
+    body: decodedBody,
+    keywords: keywordsSet,
+    favourited: false
+  }
+}
 
 function getAllItems() {
   return async (dispatch) => {
@@ -11,20 +32,7 @@ function getAllItems() {
     try {
       const rawArray = await itemService.getItems()
       const newResponse = rawArray.map((item, index) => {
-        const { body, category, title, keywords } = item
-        const decodedBody = he.decode(body)
-        const keywordsArray = keywords.split(',').map((keyword) => {
-          return keyword.trim()
-        })
-        const keywordsSet = new Set(keywordsArray)
-        return {
-          id: index,
-          title,
-          category,
-          body: decodedBody,
-          keywords: keywordsSet,
-          favourited: false
-        }
+        return transformRawItem(item, index)
       })
       return dispatch({
         type: itemConstants.GET_ALL_ITEMS_SUCCESS,
@@ -38,30 +46,17 @@ function getAllItems() {
   }
 }
 
-function updateObjectInArray(array, action) {
-  return array.map((item, index) => {
-    if (index !== action.index) {
-      // This isn't the item we care about - keep it as-is
-      return item
-    }
-
-    // Otherwise, this is the one we want - return an updated value
-    return {
-      ...item,
-      ...action.item
-    }
-  })
-}
-
 function toggleFavourite(id: number) {
   return async (dispatch, getState) => {
     const { items } = getState().itemReducers
 
     const newItems = items.map((item, index) => {
+      // If this is not the item we want to toggle, just return it
       if (index !== id) {
         return item
       }
 
+      // Otherwise toggle the favourited status
       return {
         ...item,
         favourited: !item.favourited
